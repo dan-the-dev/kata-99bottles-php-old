f712f7ffbc7745bc059bf66d0cfd4fd742ac7755
danielescillia <daniele.scillia@gmail.com>
2021-09-23 17:05:27 +0200

    feat: implemented 2 bottles verse with Obvious implementation

--- src/Kata/Song99Bottles.php
+++ src/Kata/Song99Bottles.php
@@ -7,9 +7,10 @@ class Song99Bottles
     public function verse(int $currentBottles): array
     {
         $remainingBottles = $currentBottles - 1;
+        $bottleNoun = $remainingBottles === 1 ? 'bottle' : 'bottles';
         return [
             "$currentBottles bottles of beer on the wall, $currentBottles bottles of beer.",
-            "Take one down and pass it around, {$remainingBottles} bottles of beer on the wall."
+            "Take one down and pass it around, {$remainingBottles} {$bottleNoun} of beer on the wall."
         ];
     }
 }
--- tests/Kata/Song99BottlesTest.php
+++ tests/Kata/Song99BottlesTest.php
@@ -51,7 +51,7 @@ class Song99BottlesTest extends TestCase
     public function test2BottlesVerse(): void
     {
         $expected = ["2 bottles of beer on the wall, 2 bottles of beer.", "Take one down and pass it around, 1 bottle of beer on the wall."];
-        $actual = $this->song99Bottles->verse(3);
+        $actual = $this->song99Bottles->verse(2);
 
         $this->assertSame($expected, $actual);
     }
