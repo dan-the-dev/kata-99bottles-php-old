81ed3de6831dc8dfccee1d55ccbeda671e476614
danielescillia <daniele.scillia@gmail.com>
2021-09-23 15:50:57 +0200

    feat: implemented 4 bottles verse with Obvious implementation just to get the green

--- src/Kata/Song99Bottles.php
+++ src/Kata/Song99Bottles.php
@@ -6,6 +6,13 @@ class Song99Bottles
 {
     public function verse(int $int): array
     {
+        if ($int === 4) {
+            return [
+                "4 bottles of beer on the wall, 4 bottles of beer.",
+                "Take one down and pass it around, 3 bottles of beer on the wall."
+            ];
+        }
+
         if ($int === 98) {
             return [
                 "98 bottles of beer on the wall, 98 bottles of beer.",
