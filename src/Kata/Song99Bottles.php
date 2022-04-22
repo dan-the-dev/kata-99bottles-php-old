882e9acdd3b58f41a12d37edaf8004cbfa64edb9
dan-the-dev <daniele.scillia@gmail.com>
2022-04-22 12:04:32 +0200

    refactor: refactored verse method to avoid duplication

--- src/Kata/Song99Bottles.php
+++ src/Kata/Song99Bottles.php
@@ -17,8 +17,8 @@ class Song99Bottles
     public function getVerse(): array
     {
         return [
-            '99 bottles of beer on the wall, 99 bottles of beer.',
-            'Take one down and pass it around, 98 bottles of beer on the wall.',
+            $this->getVerseFirstLine(),
+            $this->getVerseSecondLine(),
         ];
     }
 }
