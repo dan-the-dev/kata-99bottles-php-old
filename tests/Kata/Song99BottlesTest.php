4a92a91db714ba18a52a0ef35d75eefe0bfbdbbe
dan-the-dev <daniele.scillia@gmail.com>
2022-05-06 10:26:35 +0200

    test: verified verse together works

--- tests/Kata/Song99BottlesTest.php
+++ tests/Kata/Song99BottlesTest.php
@@ -128,14 +128,14 @@ class Song99BottlesTest extends TestCase
         );
     }
 
-    public function test3BottlesVerseComplete(): void
+    public function test0BottleVerseComplete(): void
     {
         $this->assertEquals(
             [
-                '3 bottles of beer on the wall, 3 bottles of beer.',
-                'Take one down and pass it around, 2 bottles of beer on the wall.',
+                'No more bottles of beer on the wall, no more bottles of beer.',
+                'Go to the store and buy some more, 99 bottles of beer on the wall.',
             ],
-            $this->song99Bottles->getVerse(3)
+            $this->song99Bottles->getVerse(0)
         );
     }
 }
