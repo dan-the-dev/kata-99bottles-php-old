edaf52d0620eba46e5f03b25ef8972358c3732d0
Daniele Scillia <danielescillia@Danieles-MacBook-Pro.local>
2023-06-09 17:53:39 +0200

    test: added test for verse 2

--- tests/Kata/Song99BottlesTest.php
+++ tests/Kata/Song99BottlesTest.php
@@ -20,17 +20,24 @@ class Song99BottlesTest extends TestCase
         $this->assertEquals($expectedVerse, $verse);
     }
 
-    public function testVerse98(): void
+    public function testVerse50(): void
     {
-        $verse = $this->song99Bottles->calculateVerseFromCurrentBottleNumber(new BottleNumber(98));
-        $expectedVerse = new Verse('98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around, 97 bottles of beer on the wall.');
+        $verse = $this->song99Bottles->calculateVerseFromCurrentBottleNumber(new BottleNumber(50));
+        $expectedVerse = new Verse('50 bottles of beer on the wall, 50 bottles of beer. Take one down and pass it around, 49 bottles of beer on the wall.');
         $this->assertEquals($expectedVerse, $verse);
     }
 
-    public function testVerse97(): void
+    public function testVerse3(): void
     {
-        $verse = $this->song99Bottles->calculateVerseFromCurrentBottleNumber(new BottleNumber(97));
-        $expectedVerse = new Verse('97 bottles of beer on the wall, 97 bottles of beer. Take one down and pass it around, 96 bottles of beer on the wall.');
+        $verse = $this->song99Bottles->calculateVerseFromCurrentBottleNumber(new BottleNumber(3));
+        $expectedVerse = new Verse('3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall.');
+        $this->assertEquals($expectedVerse, $verse);
+    }
+
+    public function testVerse2(): void
+    {
+        $verse = $this->song99Bottles->calculateVerseFromCurrentBottleNumber(new BottleNumber(2));
+        $expectedVerse = new Verse('2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.');
         $this->assertEquals($expectedVerse, $verse);
     }
 }
